a709fa3f1158ddc74c73403019bc3eece6f3927d
Jacob L Dot <jacobldot0@gmail.com>
2024-12-12 11:28:12 -0800

    ok

---
+++ src/BSTNode.java
@@ -0,0 +1,40 @@
+public class BSTNode {
+    private BSTNode left;
+    private BSTNode right;
+    private int val;
+
+    public BSTNode(int val) {
+        this.left = null;
+        this.right = null;
+        this.val = val;
+    }
+
+    /** Getters and Setters **/
+    public BSTNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BSTNode left) {
+        this.left = left;
+    }
+
+    public BSTNode getRight() {
+        return right;
+    }
+
+    public void setRight(BSTNode right) {
+        this.right = right;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public String toString() {
+        return Integer.toString(this.val);
+    }
+
+    public boolean equals(BSTNode other) {
+        return this.val == other.val;
+    }
+}
